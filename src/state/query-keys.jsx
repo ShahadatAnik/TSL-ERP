8b7dc10b0dc9d18190cc172201136aec20709f35
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2024-10-01 20:39:25 +0600

    feat: :sparkles: QueryKeys added

--- src/state/query-keys.jsx
+++ src/state/query-keys.jsx
@@ -18,3 +18,34 @@ export const adminQK = {
 	users: () => [...adminQK.all(), 'users'],
 	user: (uuid) => [...adminQK.users(), uuid],
 };
+
+export const storeQK = {
+
+	//* Stock
+	store: () => ['store'],
+	
+	stock: () => [...storeQK.store(), 'stock'],
+	stockByUUID: (uuid) => [...storeQK.stock(), uuid],
+
+	//* Article
+	article: () => [...storeQK.store(), 'article'],
+	articleByUUID: (uuid) => [...storeQK.article(), uuid],
+
+	//* Category
+	category: () => [...storeQK.store(), 'category'],
+	categoryByUUID: (uuid) => [...storeQK.category(), uuid],
+
+	//* LC
+	lc: () => [...storeQK.store(), 'lc'],
+	lcByUUID: (uuid) => [...storeQK.lc(), uuid],
+
+	//* Buyer
+	buyer: () => [...storeQK.store(), 'buyer'],
+	buyerByUUID: (uuid) => [...storeQK.buyer(), uuid],
+
+	//* Vendor
+	vendor: () => [...storeQK.store(), 'vendor'],
+	vendorByUUID: (uuid) => [...storeQK.vendor(), uuid],
+
+
+};
--- src/state/store.jsx
+++ src/state/store.jsx
@@ -1,3 +1,85 @@
 import createGlobalState from '.';
+import { storeQK } from './query-keys';
 
-import './query-keys';
+// * Buyer
+export const useStoreBuyer = () =>
+	createGlobalState({
+		queryKey: storeQK.buyer(),
+		url: '',
+	});
+
+export const useStoreBuyerByUUID = (uuid) =>
+	createGlobalState({
+		queryKey: storeQK.buyerByUUID(uuid),
+		url: '',
+		enabled: !!uuid,
+	});
+
+// * Article
+export const useStoreArticle = () =>
+	createGlobalState({
+		queryKey: storeQK.article(),
+		url: '',
+	});
+
+export const useStoreArticleByUUID = (uuid) =>
+	createGlobalState({
+		queryKey: storeQK.articleByUUID(uuid),
+		url: '',
+		enabled: !!uuid,
+	});
+
+// * Category
+export const useStoreCategory = () =>
+	createGlobalState({
+		queryKey: storeQK.category(),
+		url: '',
+	});
+
+export const useStoreCategoryByUUID = (uuid) =>
+	createGlobalState({
+		queryKey: storeQK.categoryByUUID(uuid),
+		url: '',
+		enabled: !!uuid,
+	});
+
+// * LC
+export const useStoreLC = () =>
+	createGlobalState({
+		queryKey: storeQK.lc(),
+		url: '',
+	});
+
+export const useStoreLCByUUID = (uuid) =>
+	createGlobalState({
+		queryKey: storeQK.lcByUUID(uuid),
+		url: '',
+		enabled: !!uuid,
+	});
+
+// * Vendor
+export const useStoreVendor = () =>
+	createGlobalState({
+		queryKey: storeQK.vendor(),
+		url: '',
+	});
+
+export const useStoreVendorByUUID = (uuid) =>
+	createGlobalState({
+		queryKey: storeQK.vendorByUUID(uuid),
+		url: '',
+		enabled: !!uuid,
+	});
+// * stock
+export const useStoreStock = () =>
+	createGlobalState({
+		queryKey: storeQK.stock(),
+		url: '',
+	});
+
+export const useStoreStockByUUID = (uuid) =>
+	createGlobalState({
+		queryKey: storeQK.stockByUUID(uuid),
+		url: '',
+		enabled: !!uuid,
+	});
